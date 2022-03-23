8ec3a4ad71091e06828410feabcf9158c7bc008f
ArenVosk <84827064+ArenVosk@users.noreply.github.com>
2022-03-22 19:29:00 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Has Been Loaded");
+}
